101c3d47f4d07d1911e13934026549d69f1a760a
rishuyadav <rishuss1212@gmail.com>
2022-09-26 08:08:11 +0530

    Fix: Fixed the bug of mail intent not being sent
    Signed-off-by: Rishu Yadav <rishuss1212@gmail.com>

--- app/src/main/java/com/example/konarkcomputers/NewProduct.kt
+++ app/src/main/java/com/example/konarkcomputers/NewProduct.kt
@@ -2,9 +2,8 @@ package com.example.konarkcomputers
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.konarkcomputers.databinding.ActivityNewProductBinding
 
 class NewProduct : AppCompatActivity() {
@@ -16,24 +15,20 @@ class NewProduct : AppCompatActivity() {
         binding = ActivityNewProductBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnsendnp.setOnClickListener {
-            val email = binding.emailcontainer.toString()
-            val inputext1 = binding.prod.toString()
-            val inputext22 = binding.prof.toString()
-            val inputext33 = binding.Nmodel.toString()
-            val inputext44 = binding.msg.toString()
-
-            val shareintent = Intent()
-            shareintent.action = Intent.ACTION_SEND
-
-
-            shareintent.data = Uri.parse("mailto:")
-            shareintent.putExtra(Intent.EXTRA_EMAIL, email)
-            shareintent.putExtra(Intent.EXTRA_SUBJECT, inputext1)
-            shareintent.putExtra(Intent.EXTRA_TEXT, inputext22)
-            shareintent.putExtra(Intent.EXTRA_TEXT, inputext33)
-            shareintent.putExtra(Intent.EXTRA_TEXT, inputext44)
-
-        startActivity(Intent.createChooser(shareintent,"Email to"))
+            val email = arrayOf(binding.emailcontainer.text.toString())
+            val inputext1 = binding.prod.text.toString()
+            val inputext22 = binding.prof.text.toString()
+            val inputext33 = binding.Nmodel.text.toString()
+            val inputext44 = binding.msg.text.toString()
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, email)
+                putExtra(Intent.EXTRA_SUBJECT, inputext1)
+                putExtra(Intent.EXTRA_TEXT, inputext22 + "\n" + inputext33 + "\n" + inputext44)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
     }
 }
