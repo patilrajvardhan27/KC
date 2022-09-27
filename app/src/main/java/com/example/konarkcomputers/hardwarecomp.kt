c89b1317eed8729fc9489ccd4cc43baff6cc7767
patilrajvardhan27 <patilrajvardhan27@gmail.com>
2022-09-27 08:26:00 +0530

    new changes

--- app/src/main/java/com/example/konarkcomputers/hardwarecomp.kt
+++ app/src/main/java/com/example/konarkcomputers/hardwarecomp.kt
@@ -3,9 +3,14 @@ package com.example.konarkcomputers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class hardwarecomp : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hardwarecomp)
+
+
     }
 }
