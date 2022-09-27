db24ce3288eb74adea39e08c89df2ea191c3c137
patilrajvardhan27 <patilrajvardhan27@gmail.com>
2022-09-27 14:41:37 +0530

    2nd commit of day

--- app/src/main/java/com/example/konarkcomputers/hardwarecomp.kt
+++ app/src/main/java/com/example/konarkcomputers/hardwarecomp.kt
@@ -1,16 +1,36 @@
 package com.example.konarkcomputers
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.konarkcomputers.databinding.ActivityHardwarecompBinding
+import com.example.konarkcomputers.databinding.ActivitySoftwarecompBinding
 
 
 class hardwarecomp : AppCompatActivity() {
 
-
+    lateinit var binding: ActivityHardwarecompBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hardwarecomp)
-
-
+        binding = ActivityHardwarecompBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnhardsend.setOnClickListener(){
+            val email = arrayOf(binding.emailcontainer.text.toString())
+            val inptext1 = binding.device.text.toString()
+            val inptext2 = binding.Namemodel.text.toString()
+            val inptext3 = binding.msg.text.toString()
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL,email)
+                putExtra(Intent.EXTRA_SUBJECT,inptext1)
+                putExtra(Intent.EXTRA_TEXT,inptext2 + "\n" + inptext3)
+            }
+            if (intent.resolveActivity(packageManager)!=null){
+                startActivity(intent)
+            }
+        }
     }
 }
+
+
--- app/src/main/java/com/example/konarkcomputers/softwarecomp.kt
+++ app/src/main/java/com/example/konarkcomputers/softwarecomp.kt
@@ -1,11 +1,33 @@
 package com.example.konarkcomputers
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.konarkcomputers.databinding.ActivitySoftwarecompBinding
 
 class softwarecomp : AppCompatActivity() {
+
+    lateinit var binding: ActivitySoftwarecompBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_softwarecomp)
+        binding = ActivitySoftwarecompBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnsofsend.setOnClickListener(){
+            val email = arrayOf(binding.emailcontainer.text.toString())
+            val inptext1 = binding.softprod.text.toString()
+            val inptext2 = binding.msg.text.toString()
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL,email)
+                putExtra(Intent.EXTRA_SUBJECT,inptext1)
+                putExtra(Intent.EXTRA_TEXT,inptext2)
+            }
+            if (intent.resolveActivity(packageManager)!=null){
+                startActivity(intent)
+            }
+        }
     }
 }
